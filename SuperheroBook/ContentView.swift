1c1969857959398d2413792dfa91a650910d55f2
emirhanzeyrek <emirhanzeyrekcom@gmail.com>
2024-03-07 16:59:08 +0300

    Initial Commit

---
+++ SuperheroBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperheroBook
+//
+//  Created by Emirhan Zeyrek on 7.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperheroBook/SuperheroBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroBookApp.swift
+//  SuperheroBook
+//
+//  Created by Emirhan Zeyrek on 7.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperheroBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
